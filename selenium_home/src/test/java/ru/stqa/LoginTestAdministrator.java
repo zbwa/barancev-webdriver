6129f6a0ad26d837c182411602be796cc5dd350c
Dmitri Zverev <zverevdmitri.87@gmail.com>
2017-02-26 14:21:23 +0300

    InternetExplorerDriver

--- selenium_home/src/test/java/ru/stqa/LoginTestAdministrator.java
+++ selenium_home/src/test/java/ru/stqa/LoginTestAdministrator.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -18,7 +19,7 @@ public class LoginTestAdministrator {
     private WebDriverWait wait;
     @Before
     public void setUp(){
-        driver = new ChromeDriver();
+        driver = new InternetExplorerDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
