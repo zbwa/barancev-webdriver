984c0544d11d8d94be715742349569523c6137d7
Dmitri Zverev <zverevdmitri.87@gmail.com>
2017-03-07 01:21:16 +0300

    HomeWork7

--- selenium_home/src/test/java/ru/stqa/LoginTestAdministrator.java
+++ selenium_home/src/test/java/ru/stqa/LoginTestAdministrator.java
@@ -1,42 +1,44 @@
 package ru.stqa;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
 
-import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Created by Zhanna on 25.02.2017.
  */
-public class LoginTestAdministrator {
-    private WebDriver driver;
-    private WebDriverWait wait;
-    @Before
-    public void setUp(){
-        DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
-        capabilities.setCapability(InternetExplorerDriver.REQUIRE_WINDOW_FOCUS, true);
-        driver = new InternetExplorerDriver(capabilities);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 10);
+public class LoginTestAdministrator extends TestBase{
+    public boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
     @Test
-    public void loginTest(){
+    public void loginTest()throws Exception{
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-    }
-
-    @After
-    public void tearDown(){
-        driver.quit();
-        driver = null;
+        int size = driver.findElements(By.cssSelector("li#app-")).size();
+        List<WebElement> list;
+        for (int i = 0; i < size; i++){
+            list = driver.findElements(By.cssSelector("li#app-"));
+            list.get(i).click();
+            int subSize = driver.findElements(By.cssSelector("[id |= doc]")).size();
+            List<WebElement> subList;
+            for (int j = 0; j < subSize; j++){
+                subList = driver.findElements(By.cssSelector("[id |= doc]"));
+                subList.get(j).click();
+                isElementPresent(By.xpath("//h1"));
+            }
+            isElementPresent(By.xpath("//h1"));
+        }
     }
 }
--- selenium_home/src/test/java/ru/stqa/TestBase.java
+++ selenium_home/src/test/java/ru/stqa/TestBase.java
@@ -4,37 +4,27 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import ru.stqa.selenium.factory.ThreadLocalSingleWebDriverPool;
-import ru.stqa.selenium.factory.WebDriverPool;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Zhanna on 26.02.2017.
  */
 public class TestBase {
-    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
     public WebDriver driver;
     public WebDriverWait wait;
+
     @Before
     public void setUp(){
-        if (tlDriver.get() != null) {
-            driver = tlDriver.get();
-            wait = new WebDriverWait(driver, 10);
-            return;
-        }
-
         driver = new ChromeDriver();
-        tlDriver.set(driver);
-        wait = new WebDriverWait(driver, 10);
-
-        Runtime.getRuntime().addShutdownHook(
-                new Thread(() -> { driver.quit(); driver = null; }));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 5);
     }
 
     @After
     public void tearDown(){
-        /*driver.quit();
-        driver = null;*/
+        driver.quit();
+        driver = null;
     }
 }
