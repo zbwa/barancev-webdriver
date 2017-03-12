a091ced029f87acd94a88e50c2879420f41c8346
Dmitri Zverev <zverevdmitri.87@gmail.com>
2017-03-12 22:54:33 +0300

    NewCustomer

--- selenium_home/src/test/java/ru/stqa/NewCustomer.java
+++ selenium_home/src/test/java/ru/stqa/NewCustomer.java
@@ -1,31 +1,39 @@
 package ru.stqa;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.NoSuchElementException;
 
 /**
  * Created by Zhanna on 12.03.2017.
+ * Тест для задания №11
  */
 public class NewCustomer extends TestBase{
-    public boolean isElementPresent(By locator){
-        try {
-            driver.findElement(locator);
-            return true;
-        }
-        catch (NoSuchElementException e){
-            return false;
-        }
-    }
+
     @Test
-    public void loginPage(){
+    public void loginPage() throws Exception{
         driver.get("http://localhost/litecart/en/");
         driver.findElement(By.cssSelector("a[href*='create_account']")).click();
-        isElementPresent(By.cssSelector("h1"));
-        System.out.println(driver.findElement(By.cssSelector("h1")).getText());
-        Assert.assertEquals("Create Account", driver.findElement(By.cssSelector("h1")).getText());
+        driver.findElement(By.cssSelector("input[name=firstname]")).sendKeys("Hannibal");
+        driver.findElement(By.cssSelector("input[name=lastname]")).sendKeys("Gannibal");
+        driver.findElement(By.cssSelector("input[name=address1]")).sendKeys("Hollywood");
+        driver.findElement(By.cssSelector("input[name=postcode]")).sendKeys("19722");
+        driver.findElement(By.cssSelector("input[name=city]")).sendKeys("Los=Angeles");
+        Select select_country = new Select(driver.findElement(By.cssSelector("select[name=country_code]")));
+        select_country.selectByVisibleText("United States");
+        Select select_zone = new Select(driver.findElement(By.cssSelector("select[name=zone_code]")));
+        select_zone.selectByVisibleText("California");
+        driver.findElement(By.cssSelector("input[name=email]")).sendKeys("HannibalGannibal@yandex.ru");
+        driver.findElement(By.cssSelector("input[name=phone]")).sendKeys("+1800555-55-55");
+        driver.findElement(By.cssSelector("input[name=password]")).sendKeys("123");
+        driver.findElement(By.cssSelector("input[name=confirmed_password]")).sendKeys("123");
+        driver.findElement(By.cssSelector("button[name=create_account]")).click();
+        driver.findElement(By.cssSelector("a[href*='logout']")).click();
+        driver.findElement(By.cssSelector("input[name=email]")).sendKeys("HannibalGannibal@gmail.ru");
+        driver.findElement(By.cssSelector("input[name=password]")).sendKeys("123");
+        driver.findElement(By.cssSelector("button[name=login]")).click();
+        driver.findElement(By.cssSelector("a[href*='logout']")).click();
     }
 }
